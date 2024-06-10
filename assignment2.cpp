ac378a906442480e869742522c36e7a489740969
jatinsinghal4456 <131702939+jatinsinghal4456@users.noreply.github.com>
2024-06-10 17:36:02 +0530

    Add files via upload

---
+++ assignment2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{ // Question 1
+    // ans is 1
+
+    // Q2
+    // Output is : 0 1
+
+    // Q3
+    // x = 2
+
+    // Q4
+    int radius, height;
+    cout << "Enter the radius and height of cylinder : ";
+    cin >> radius >> height;
+    cout << "Volumne of cylinder is " << 2 * 3.14 * radius * height;
+
+    // Q5
+    char a, b;
+    cout << "Enter two characters as input : ";
+    cin >> a >> b;
+    cout << "Difference of there ASCII valure is  " << int(a - b);
+
+    // Q6
+    // Output is 49
+}
