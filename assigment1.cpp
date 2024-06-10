16366459cc935020a2d90591049e5301cbe43994
jatinsinghal4456 <131702939+jatinsinghal4456@users.noreply.github.com>
2024-06-10 17:35:28 +0530

    Add files via upload

---
+++ assigment1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Question 1
+    cout << "Physics" << endl;
+    cout << "wallah" << endl;
+
+    // Question 2
+    cout << 5 + 5 << endl;
+
+    // Q3
+    // int 4 btyes;
+    // bool 1 bytes;
+    // float 4 bytes ;
+
+    // Q4
+    //  a =6  , b=4
+
+    // Q5
+    int radius = 10;
+    int circumference = 2 * 3.14 * 10;
+
+    // Q6
+    // only _FLOAT is correct
+}
